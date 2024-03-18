cf63724e768f25eea3e324623dbc4d42544b0bc2
Ishan09811 <156402647+Ishan09811@users.noreply.github.com>
2024-03-18 19:59:38 +0530

    bonk

--- renderer/command/data_source/pcm_float.cpp
+++ renderer/command/data_source/pcm_float.cpp
@@ -19,6 +19,12 @@ void PcmFloatDataSourceVersion1Command::Dump(const ADSP::CommandListProcessor& p
 void PcmFloatDataSourceVersion1Command::Process(const ADSP::CommandListProcessor& processor) {
     auto out_buffer = processor.mix_buffers.subspan(output_index * processor.sample_count,
                                                     processor.sample_count);
+  for (auto& wave_buffer : wave_buffers) {
+        wave_buffer.loop_start_offset = wave_buffer.start_offset;
+        wave_buffer.loop_end_offset = wave_buffer.end_offset;
+        wave_buffer.loop_count = wave_buffer.loop ? -1 : 0;
+    }
+  
 
     DecodeFromWaveBuffersArgs args{
         .sample_format{SampleFormat::PcmFloat},
